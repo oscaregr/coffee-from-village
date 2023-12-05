362ec8cb2c8acc962bd72e5203c4abf8f4544200
oscaregr <bavuey@hotmail.com>
2023-12-04 23:29:42 -0600

    Add structure, header nav and design.
    In this commit the structure of the project is defined.
    Add the store logo.
    First design for the header home page.
    Implement partials in the project navigation.
    Defind the color palette for the web page.
    Use separate sections of CSS and create the first style variables.

---
+++ app/partials/nav_menu.php
@@ -0,0 +1,14 @@
+<div class="menu">
+  <input id="menu__toggle" type="checkbox" />
+  <label class="menu__btn" for="menu__toggle">
+    <span></span>
+  </label>
+
+  <ul class="menu__box">
+    <li><a class="menu__item" href="#">Home</a></li>
+    <li><a class="menu__item" href="#">Productos</a></li>
+    <li><a class="menu__item" href="#">Beneficios </a></li>
+    <li><a class="menu__item" href="#">Ubicación</a></li>
+    <li><a class="menu__item" href="#">Contacto</a></li>
+  </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Café del pueblo</title>
+  <link rel="stylesheet" href="app/assets/css/styles.css">
+</head>
+<body>
+  <header>
+    <nav class="nav">
+      <section class="logo_section">
+        <img src="app/assets/img/comala_plaza.jpg" alt="" class="logo_background">
+        <div class="logo_container">
+          <img src="app/assets/img/logo.svg" alt="Café del pueblo" class="logo_image">
+          <p class="logo_text"></p>
+        </div>
+      </section>
+
+      <?php include 'app/partials/nav_menu.php'; ?>
+      
+    </nav>
+  </header>
+
+  <main class="main"></main>
+  
+  <footer class="footer">
+    <a href="" class="footer_logo">
+      <img class="nav_logo_icon" src="app/assets/img/logo.svg" alt="Café del pueblo"></img>
+    </a>
+  </footer>
+</body>
+</html>
